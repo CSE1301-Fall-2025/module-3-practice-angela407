40673f451656c028f7cd96453944fe63577942a9
Angela <a.c.lee@wustl.edu>
2025-09-24 17:44:17 -0500

    ugh

--- src/pastexam/ExamOneArrayExamples.java
+++ src/pastexam/ExamOneArrayExamples.java
@@ -16,5 +16,12 @@ public class ExamOneArrayExamples { // array examples
         2D array with initializer list*/
         System.out.println("Letter in row 1, col 0: " + randLetters[1][0]); /*
         prints out: Letter in row 1, col 0: A */
+        int[][] tired = {{1, 2, 3}, {4, 5, 6}};
+        for (int row = 0; row < tired.length; row++){ // nested for loop 2D example
+            for (int col = 0; col < tired[0].length; col++){
+                System.out.println(tired[row][col]); /* print out everything
+                in 2D array: 1,2,3,4,5,6 */
+            }
+        }
     }
 }
